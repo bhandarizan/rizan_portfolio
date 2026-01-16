1e87edd3de8ef8e1357771862a7b889f690ee869
rizan <bhandarizan129@gmail.com>
2026-01-16 23:58:23 +0545

    Resume File updated

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -35,7 +35,7 @@ export const AboutSection = () => {
               </a>
 
               <a
-                href="/resume_latest_RizanBhandari.pdf"
+                href="/resume_latest_RIzanBhandari.pdf"
                 download="RizanBhandari_Resume.pdf"
                 className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300"
               >
